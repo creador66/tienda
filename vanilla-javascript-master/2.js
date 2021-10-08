459f1c53bb2cccbc90a508f143ad3531775e546e
creador66 <87046981+creador66@users.noreply.github.com>
2021-10-07 20:09:06 -0500

    Create 2.js

---
+++ vanilla-javascript-master/2.js
@@ -0,0 +1,39 @@
+>(function(d, t, e, m){
+    
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){
+                
+        RW.init({
+            huid: "477967",
+            uid: "b8f5b8565d7f1de2d6fadeac17020678",
+            source: "website",
+            options: {
+                "advanced": {
+                    "font": {
+                        "type": "arial"
+                    },
+                    "text": {
+                        "rateThis": "!"
+                    }
+                },
+                "size": "tiny",
+                "type": "nero",
+                "lng": "es",
+                "style": "thumbs",
+                "isDummy": false
+            } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
